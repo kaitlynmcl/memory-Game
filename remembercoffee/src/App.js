1721bf5a6022f420b7a15981f2b1eb579c8de301
Kaitlyn McLaughlin <kaitlyn@Kaitlyns-MacBook-Air.local>
2018-12-26 23:36:50 -0500

    title

--- remembercoffee/src/App.js
+++ remembercoffee/src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nav from "./components/Nav";
+
+import Title from "./components/Title";
 import Jumbotron from "./components/Jumbotron";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Nav />
+        <Nav 
+         title="Coffee Brain: Memory Game">
+        </Nav>
           <Jumbotron />
             
 
