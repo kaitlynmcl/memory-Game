b2e28be872d5b484b6d8bb58a1f7fff88ebcf35b
Kaitlyn McLaughlin <kaitlyn@Kaitlyns-MacBook-Air.local>
2019-01-03 12:07:53 -0500

    click function works

--- remembercoffee/src/App.js
+++ remembercoffee/src/App.js
@@ -16,40 +16,113 @@ import pic10 from "./components/Nav/images/pic10.jpg";
 import pic11 from "./components/Nav/images/pic11.jpg";
 import pic12 from "./components/Nav/images/pic12.jpg";
 
-                                            // LOADS ARRAY OF IMAGES FOR DISPLAY
+                                          // LOADS ARRAY OF IMAGES FOR DISPLAY
 
 const images = [pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9, pic10, pic11, pic12];
 
-                                            // SETS ORIGINAL STATE OF GAME 
+                                          // SETS ORIGINAL STATE OF GAME
 class App extends Component {
   state = {
     score: 0,
     
     topScore: 0,
     array: [],
-    guessedCorrect: "true",
-    guessedIncorrect: "false"
+    correct: "true",
+    incorrect: "false"
   };
+
+  shuffleImgs = arr => {                  // SHUFFLES THROUGH IMAGES TO DISPLAY BEST RESULTS
+    for (let i = arr.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+  };
+
+  handleClickImage = elem => {            // EVENT HANDLER FOR CLICKING ON IMAGES
+    var newArray = [...this.state.array]; // CREATES NEW ARRAY TO ITERATE THROUGH CHANGING STATE
+
+    if (newArray.length === 0) {
+      newArray.push(elem);
+      this.setState({
+        correct: "true"
+      });
+      if (this.state.score >= this.state.topScore) {
+        this.setState({
+          score: this.state.score + 1,
+          topScore: this.state.topScore + 1,
+          array: newArray,
+          correct: "true"
+
+        });
+        console.log("%cyou guessed correctly", "color: green");
+      } else {
+        this.setState({
+          score: this.state.score + 1,
+          topScore: this.state.topScore,
+          array: newArray
+        });
+        console.log("%cyou guessed correctly", "color: green");
+      }
+    } else {
+      const found = newArray.includes(elem);
+
+      if (!found) {
+        newArray.push(elem);
+        if (this.state.score >= this.state.topScore) {
+          this.setState({
+            score: this.state.score + 1,
+            topScore: this.state.topScore + 1,
+            array: newArray,
+            correct: "true"
+          });
+          console.log("%cyou guessed correctly", "color: green");
+        } else {
+          this.setState({
+            score: this.state.score + 1,
+            topScore: this.state.topScore,
+            array: newArray,
+            correct: "true"
+
+          });
+          console.log("%cyou guessed correctly", "color: green");
+        }
+
+      } else {
+        this.setState({
+          score: 0,
+          topScore: this.state.topScore,
+          array: [],
+          correct: "false"
+        });
+        console.log("%cyou guessed incorrectly", "color: red");
+      }
+    }
+
+    console.log(newArray);
+    this.shuffleImgs(images);
+  };
+
 
   render() {                                // RENDERS NAV, JUMBOTRON, AND FOOTER 
     return (
       <div>
-        <Nav 
-         title="Coffee Brain:">
+        <Nav
+          title="Coffee Brain:">
         </Nav>
         <Jumbotron />
+        {/* DISPLAYS IMAGES IN JUMBOTRON  */}
+        <div className="container">
+          {images.map((pic) => (<Jumbotron
+            image={pic}
+            handleClickImage={this.handleClickImage}
+            key={pic}
+          />))}
 
-          <div className="container">
-            {images.map((pic) => (<Jumbotron
-              image={pic}
-              handleClickImage={this.handleClickImage}
-              key={pic}
-            />))}
-
-           </div>
-           <Footer />
         </div>
- 
+        <Footer />
+      </div>
+
     );
   }
 }
--- remembercoffee/src/components/Jumbotron/Jumbotron.js
+++ remembercoffee/src/components/Jumbotron/Jumbotron.js
@@ -1,18 +1,15 @@
 import React from "react";
 import "./Jumbotron.css";
 
-const Jumbotron = props => (
 
-  <div
-    className="card"
-    value={props.id}
-    onClick={() => props.handleClick(props.id)}
-  >
-    <div className="img-container">
-      <img alt={props.name} src={props.image} />
-      
-    </div>
-  </div>
-);
+const Jumbotron = props =>  {
+    return (
+            <div className="img-container">
+                <div className="card-body">
+                    <img alt={props.image} src={props.image} onClick={() => props.handleClickImage(props.image)}/>
+                </div>
+            </div>
+    )
+};
 
 export default Jumbotron;
